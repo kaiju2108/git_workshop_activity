5a39f637d4f03c8bc1fa3f834bac282bd5ed9bf9
kaiju2108 <ryantanyx21@gmail.com>
2026-03-23 13:35:21 +1100

    test

--- top_level.v
+++ top_level.v
@@ -2,20 +2,20 @@ module top_level (
 	/* Add your inputs here */
 	
 	// Clock
-		
+		input wire CLOCK_50,
 	// Keys
-		
+		input wire [0:0]KEY,
 	// Leds
-	
+		output wire [9:0] LEDR,
 	// Switches
-	
+		input wire [9:0] SW
 );
 
     nios_system u0 (
-        .clk_clk       (/* Connect the clock */),       
-        .reset_reset_n (/* Connect a Key for reset */), 
-        .ledr_export   (/* Connect all LEDs */),   
-        .sw_export     (/* Connect all Switches */)      
+        .clk_clk       (CLOCK_50/* Connect the clock */),       
+        .reset_reset_n (KEY[0]/* Connect a Key for reset */), 
+        .ledr_export   (LEDR[9:0]/* Connect all LEDs */),   
+        .sw_export     (SW[9:0]/* Connect all Switches */)      
     );
 
 	 
